9eadc69023eea8d942a6748ea2040d53e13a7a77
hsvshzvbxj-del <hsvshzvbxj@gmail.com>
2026-06-23 14:54:55 +0300

    Add server/src/models/Discussion.ts

---
+++ server/src/models/Discussion.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IComment {
+  _id: mongoose.Types.ObjectId;
+  authorId: mongoose.Types.ObjectId;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface IDiscussion extends Document {
+  title: string;
+  content: string;
+  tags: string[];
+  authorId: mongoose.Types.ObjectId;
+  likesCount: number;
+  likes: mongoose.Types.ObjectId[];
+  saves: mongoose.Types.ObjectId[];
+  isPinned: boolean;
+  comments: IComment[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const CommentSchema = new Schema<IComment>({
+  authorId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  content: { type: String, required: true },
+}, { timestamps: true });
+
+const DiscussionSchema = new Schema<IDiscussion>({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  tags: [String],
+  authorId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  likesCount: { type: Number, default: 0 },
+  likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  saves: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  isPinned: { type: Boolean, default: false },
+  comments: [CommentSchema],
+}, { timestamps: true });
+
+export const Discussion = mongoose.model<IDiscussion>('Discussion', DiscussionSchema);
